a56e0120f7209486493b2ac0a70d382976b36b3f
willdespegar <asalazar@despegar.com>
2014-10-14 11:45:38 -0300

    Flight Result Binding

--- Despegar.WP.UI.Models/FlightResultsModel.cs
+++ Despegar.WP.UI.Models/FlightResultsModel.cs
@@ -1,14 +1,18 @@
-﻿using System;
+﻿using Despegar.Core.Business.Flight.Itineraries;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
 
 namespace Despegar.WP.UI.Model
 {
-    public class FlightResultsModel: AppModelBase , Interfaces.IInitializeModelInterface, Interfaces.IValidateInterface
+    public class FlightResultsModel : AppModelBase, Interfaces.IInitializeModelInterface, Interfaces.IValidateInterface
     {
-
+        public FlightsItineraries Itineraries{ get; set; }
+        
         public FlightResultsModel()
         {
             this.InitializeModel();
@@ -17,15 +21,16 @@ namespace Despegar.WP.UI.Model
         public new void InitializeModel()
         {
             base.InitializeModel();
-        
+
             //TODO initialize all variables needed for this page.
         }
 
         public new void Validate()
         {
             base.Validate();
-            
+
             //Validate each variable for this model
         }
+
     }
 }
