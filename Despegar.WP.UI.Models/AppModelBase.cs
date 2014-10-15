b5008d45b9f387742d8b3d0c78fa2baa3609664b
willdespegar <asalazar@despegar.com>
2014-10-15 10:13:16 -0300

    Binding List to listbox

--- Despegar.WP.UI.Models/AppModelBase.cs
+++ Despegar.WP.UI.Models/AppModelBase.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using Despegar.WP.UI.Model.Classes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Despegar.WP.UI.Model
 {
-    public abstract class AppModelBase 
+    public abstract class AppModelBase: INotifyPropertyChanged 
     {
+        string appName;
 
         internal AppModelBase()
         {
@@ -21,7 +24,18 @@ namespace Despegar.WP.UI.Model
 
         internal void InitializeModel()
         {
-            throw new NotImplementedException();
+            
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
     }
 }
