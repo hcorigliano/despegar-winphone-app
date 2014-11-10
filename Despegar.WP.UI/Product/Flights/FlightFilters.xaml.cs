9e6bc8cabc4de44ef4885500f40df955166bf04e
willdespegar <asalazar@despegar.com>
2014-11-10 11:04:54 -0300

    filters back action

--- Despegar.WP.UI/Product/Flights/FlightFilters.xaml.cs
+++ Despegar.WP.UI/Product/Flights/FlightFilters.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Despegar.WP.UI.Common;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -22,9 +23,14 @@ namespace Despegar.WP.UI.Product.Flights
     /// </summary>
     public sealed partial class FlightFilters : Page
     {
+        private NavigationHelper navigationHelper;
+
         public FlightFilters()
         {
             this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
+            this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
         }
 
         /// <summary>
@@ -35,6 +41,58 @@ namespace Despegar.WP.UI.Product.Flights
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             this.DataContext = e.Parameter;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="NavigationHelper"/> associated with this <see cref="Page"/>.
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session.  The state will be null the first time a page is visited.</param>
+        private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/></param>
+        /// <param name="e">Event data that provides an empty dictionary to be populated with
+        /// serializable state.</param>
+        private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        private void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (navigationHelper.CanGoBack())
+            {
+                navigationHelper.GoBack();
+            }
+        }
+
+        private void AppBarCancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO Restore first state
+            if (navigationHelper.CanGoBack())
+            {
+                navigationHelper.GoBack();
+            }
         }
     }
 }
