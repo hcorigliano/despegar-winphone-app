c86955cf7d439f4ea034878d0c5560a2427af69e
Ariel.Burgos <aburgos@despegar.com>
2014-12-01 16:40:04 -0300

    no message

--- Despegar.WP.UI.Models/FlightsCheckoutModel.cs
+++ Despegar.WP.UI.Models/FlightsCheckoutModel.cs
@@ -99,14 +99,14 @@ namespace Despegar.WP.UI.Model
             IsLoading = true;
 
             BookingFieldPost book = new BookingFieldPost();
-            //book.inbound_choice = CrossParameters.Inbound.choice;
-            //book.outbound_choice = CrossParameters.Outbound.choice;
-            //book.itinerary_id = CrossParameters.FlightId;//"prism_AR_0_FLIGHTS_A-1_C-0_I-0_RT-BUEMIA20141110-MIABUE20141111_xorigin-api!0!C_1212636001_843603426_-2008006059_1555498055_-278056197_804297563!1,6_1,4_1,5_1,2_1,3_1,1";
+            book.inbound_choice = CrossParameters.Inbound.choice;
+            book.outbound_choice = CrossParameters.Outbound.choice;
+            book.itinerary_id = CrossParameters.FlightId;//"prism_AR_0_FLIGHTS_A-1_C-0_I-0_RT-BUEMIA20141110-MIABUE20141111_xorigin-api!0!C_1212636001_843603426_-2008006059_1555498055_-278056197_804297563!1,6_1,4_1,5_1,2_1,3_1,1";
             
             //MOCKED
-            book.inbound_choice = 1;
-            book.outbound_choice = 1;
-            book.itinerary_id = "prism_AR_0_FLIGHTS_A-1_C-0_I-0_RT-BUEMIA20150610-MIABUE20150611_xorigin-api!0!C_2075990976!1,1";
+            //book.inbound_choice = 1;
+            //book.outbound_choice = 1;
+            //book.itinerary_id = "prism_AR_0_FLIGHTS_A-1_C-0_I-0_RT-BUEMIA20150610-MIABUE20150611_xorigin-api!0!C_2075990976!1,1";
 
             bookingfields = await flightService.GetBookingFields(book);
 
