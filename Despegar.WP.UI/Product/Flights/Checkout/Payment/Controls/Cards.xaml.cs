fd6b8a15b62f3a61cdfc89dd53475156733731b5
Ariel.Burgos <aburgos@despegar.com>
2014-12-22 12:57:05 -0300

    Added image load fail to credit cards

--- Despegar.WP.UI/Product/Flights/Checkout/Payment/Controls/Cards.xaml.cs
+++ Despegar.WP.UI/Product/Flights/Checkout/Payment/Controls/Cards.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
@@ -29,7 +30,13 @@ namespace Despegar.WP.UI.Product.Flights.Checkout.Payment.Controls
 
         private void Image_Load_Failed(object sender, ExceptionRoutedEventArgs e)
         {
-            //((Image)sender).Source = new ImageSource("");
+            //var test = ((Image)sender).Source;
+            //int i = 1;
+            //((Image)sender).Source = new ImageSource
+            ((Image)sender).Source = new BitmapImage(new Uri("ms-appx:/Assets/Icon/CreditCard/GRL.png", UriKind.Absolute));//BitmapImage(new Uri(@"/Assets/Icon/CreditCard/GRL.png", UriKind.RelativeOrAbsolute));
+            //File.Exists("path to file")
+            //int i = 1;
+            //((Image)sender).SetValue( //Source = new ImageSource()
         }
     }
 }
