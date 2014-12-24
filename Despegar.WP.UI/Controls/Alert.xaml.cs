4ec9f5b5411a3ffce0178f5fd2196e877f7b9db7
hernan corigliano <hcorigliano@despegar.com>
2014-12-24 14:05:29 -0300

    risk questions UI

---
+++ Despegar.WP.UI/Controls/Alert.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Despegar.WP.UI.Controls
+{
+    public sealed partial class Alert : UserControl
+    {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(Alert), new PropertyMetadata(null));
+
+        #region ** BoilerPlate Code **
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void SetValueAndNotify(DependencyProperty property, object value, [CallerMemberName] string p = null)
+        {
+            SetValue(property, value);
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+        }
+        #endregion
+
+        // Bindable Property from XAML
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set
+            {
+                SetValueAndNotify(TextProperty, value);
+            }
+        }
+
+        public Alert()
+        {
+            this.InitializeComponent();
+            (this.Content as FrameworkElement).DataContext = this;
+        }
+    }
+}
