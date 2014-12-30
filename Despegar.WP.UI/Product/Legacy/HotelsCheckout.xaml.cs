e94f0b539b6bb323c6f146c994c4cf4116122ce0
Ariel.Burgos <aburgos@despegar.com>
2014-12-30 16:36:22 -0300

    fix image in hotels

--- Despegar.WP.UI/Product/Legacy/HotelsCheckout.xaml.cs
+++ Despegar.WP.UI/Product/Legacy/HotelsCheckout.xaml.cs
@@ -34,7 +34,7 @@ namespace Despegar.View
             InitializeComponent();
             
             #if DECOLAR
-            MainLogo.Source = new BitmapImage(new Uri("/Assets/Image/decolar-logo.png", UriKind.RelativeOrAbsolute));
+            MainLogo.Source = new BitmapImage(new Uri("ms-appx:/Product/Legacy/Assets/Image/decolar-logo.png", UriKind.Absolute));
             #endif
 
             if (!NetworkInterface.GetIsNetworkAvailable())
