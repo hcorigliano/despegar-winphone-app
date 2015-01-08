a9c708747f75fb570f393c75e321c2f8b505ca0b
Ariel.Burgos <aburgos@despegar.com>
2015-01-08 13:11:58 -0300

    IBugTracker File

---
+++ Despegar.Core.Log/IBugTracker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Despegar.Core.Log
+{
+    public interface IBugTracker
+    {
+        void LeaveBreadcrumb(string breadcrumb);
+
+        void LogException(Exception exception);
+
+        void LogException(Exception exception, object extrasExtraDataList);
+
+        void SetExtraData(string key, string value);
+
+        void LogEvent(string eventName);
+
+        //Log url
+        void LogURL(string url);
+
+    }
+}
