5e0e069b9806fdceda60a8f0a8c70fec58226902
willdespegar <asalazar@despegar.com>
2015-01-21 19:48:55 -0300

    PhotoGallery files

---
+++ Despegar.WP.UI/Controls/PhotoGallery/PhotoGalleryControl.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Despegar.WP.UI.Controls.PhotoGallery
+{
+    public sealed partial class PhotoGalleryControl : UserControl
+    {
+        //TODO create property to set the size of picture
+        static string URLCONTENT = "http://staticontent.com/media/pictures/{0}/118x118";
+
+        public PhotoGalleryControl()
+        {    
+            this.InitializeComponent();
+        }
+
+        private void VariableSizedWrapGrid_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+        {
+            if (args.NewValue != null)
+            {
+                List<string> keyList = new List<string>();
+                keyList.AddRange(args.NewValue as List<string>);
+
+
+                foreach (string key in keyList)
+                {
+
+                    string urlimage = String.Format(URLCONTENT, key);
+
+                    Uri imageURI = new Uri(urlimage, UriKind.Absolute);
+                    BitmapImage bmi = new BitmapImage();
+                    bmi.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+                    bmi.UriSource = imageURI;
+                    
+
+                    Grid gridvariable = sender as Grid;
+                    var imagesItems = gridvariable.Children.Where(r => r.GetType() == typeof(Image));
+
+                    foreach (Image item in imagesItems)
+                    {
+                        if (item.Source==null)
+                        {
+                            item.Source = bmi;
+                            item.Tag = key;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
