8801e0dc5e07f5e7c263be94f118284ef8347802
Ariel Groesman <ariel.groesman@746ITN008589.ar.infra.d>
2015-01-23 17:33:12 -0300

    Hotel search

---
+++ Despegar.WP.UI.Models/ViewModel/Hotels/RoomsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Despegar.Core.Log;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Despegar.WP.UI.Model.ViewModel.Hotels
+{
+    public class RoomsViewModel : ViewModelBase
+    {
+
+        private int roomsSelectedOpotion;
+        public int RoomsSelectedOpotion { get; set; }
+
+        public int Rooms { get; set; }
+
+        public RoomsViewModel(IBugTracker t) : base(t)
+        {
+            this.Rooms = 1;
+        }
+    }
+}
