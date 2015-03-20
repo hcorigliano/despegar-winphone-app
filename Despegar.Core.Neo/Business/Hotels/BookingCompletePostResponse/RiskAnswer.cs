06b0aecb8038fab387a59a6a1bf7bc0e3d7cf3bf
willdespegar <asalazar@despegar.com>
2015-03-20 16:58:28 -0300

    Risk Questions Hotels

---
+++ Despegar.Core.Neo/Business/Hotels/BookingCompletePostResponse/RiskAnswer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Despegar.Core.Neo.Business.Hotels.BookingCompletePostResponse
+{
+    public class RiskAnswer
+    {
+        public string question_id { get; set; }
+        public string answer_id { get; set; }
+        public string text { get; set; }
+    }
+}
--- Despegar.Core.Neo/Business/Hotels/BookingCompletePostResponse/RiskQuestion.cs
+++ Despegar.Core.Neo/Business/Hotels/BookingCompletePostResponse/RiskQuestion.cs
@@ -7,12 +7,37 @@ namespace Despegar.Core.Neo.Business.Hotels.BookingCompletePostResponse
 {
     public class RiskQuestion
     {
+        //public string id { get; set; }
+        //public string order { get; set; }
+        //public string title { get; set; }
+        //public string category { get; set; }
+        //public bool mandatory { get; set; }
+        //public bool free_text { get; set; }
+        //public List<Answer> answers { get; set; }
+
         public string id { get; set; }
         public string order { get; set; }
         public string title { get; set; }
         public string category { get; set; }
-        public bool mandatory { get; set; }
-        public bool free_text { get; set; }
-        public List<Answer> answers { get; set; }
+        public string mandatory { get; set; }
+        public string free_text { get; set; }
+        public FreeTextDescription free_text_description { get; set; }
+        public List<Answers> answers { get; set; }
+        public RiskAnswer risk_answer { get; set; }
+
+        private Answers selectedChoice;
+        public Answers SelectedChoice { get { return selectedChoice; } 
+            set 
+             {
+                selectedChoice = value; 
+                risk_answer.answer_id = value.id;
+                risk_answer.text = value.text;
+            }
+        }
+
+        public RiskQuestion()
+        {
+            this.risk_answer = new RiskAnswer();
+        }
     }
 }
