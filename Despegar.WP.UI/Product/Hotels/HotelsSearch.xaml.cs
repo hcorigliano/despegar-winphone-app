0f6b118f5e44ad774a3cfaf10a58592b41412706
Ariel.Burgos <aburgos@despegar.com>
2015-04-01 16:43:57 -0300

    Minor fix

--- Despegar.WP.UI/Product/Hotels/HotelsSearch.xaml.cs
+++ Despegar.WP.UI/Product/Hotels/HotelsSearch.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Despegar.Core.Neo.Business.CustomErrors;
 using Despegar.Core.Neo.InversionOfControl;
+using Despegar.WP.UI.Common;
 using Despegar.WP.UI.Controls;
 using Despegar.WP.UI.Model.Common;
 using Despegar.WP.UI.Model.ViewModel;
